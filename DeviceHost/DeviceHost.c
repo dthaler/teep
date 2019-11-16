f742e2fc870f908354b24f4c808a9fa67cf7845e
Dave Thaler <dthaler@microsoft.com>
2019-11-16 21:05:48 +0800

    Make DeviceHost get default TAM URI on command line
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- DeviceHost/DeviceHost.c
+++ DeviceHost/DeviceHost.c
@@ -3,23 +3,36 @@
 #include "../TeepAgentBrokerLib/TeepAgentBrokerLib.h"
 
 //#define DEFAULT_TAM_URI "http://127.0.0.1:12345/TEEP"
-#define DEFAULT_TAM_URI "http://localhost:54321/TEEP"
+//#define DEFAULT_TAM_URI "http://localhost:54321/TEEP"
+//#define DEFAULT_TAM_URI "http://192.168.11.23:54321/TEEP"
+//#define DEFAULT_TAM_URI "http://192.168.11.10:8888/api/tam"
+
+#define DEFAULT_TA_ID "X" // TODO: default to none
 
 int main(int argc, char** argv)
 {
-    const char* taNeeded;
-    if (argc > 1) {
-        taNeeded = argv[1];
-    } else {
-        taNeeded = "X";
+    if (argc < 2) {
+        printf("Usage: DeviceHost <TAM URI> [<TA ID>]\n");
+        printf("       where <TAM URI> is the default TAM URI to use\n");
+        printf("             <TA ID> is the TA to request (%s if none specified)\n", DEFAULT_TA_ID);
+        return 0;
     }
+
+    const char* defaultTamUri = argv[1];
+    printf("Using default TAM URI: %s\n", defaultTamUri);
+
+    const char* taNeeded = DEFAULT_TA_ID;
+    if (argc > 2) {
+        taNeeded = argv[2];
+    }
+    printf("Using TA ID: %s\n", taNeeded);
 
     int err = StartAgentBroker();
     if (err != 0) {
         return err;
     }
 
-    err = AgentBrokerRequestTA(taNeeded, DEFAULT_TAM_URI);
+    err = AgentBrokerRequestTA(taNeeded, defaultTamUri);
     if (err != 0) {
         goto exit;
     }

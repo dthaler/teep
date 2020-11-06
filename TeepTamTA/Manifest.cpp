9729b71d2b8951ce85aa2ad91ef59600214b09a4
Dave Thaler <dthaler@ntdev.microsoft.com>
2020-11-05 20:19:23 -0800

    Support multiple manifests on TAM
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

--- TeepTamTA/Manifest.cpp
+++ TeepTamTA/Manifest.cpp
@@ -15,6 +15,7 @@ Manifest::Manifest(oe_uuid_t component_id, const char* manifest, size_t manifest
     this->_manifest_size = 0;
     this->_manifest = nullptr;
     this->_component_id = component_id;
+    this->Next = nullptr;
 
     void* buffer = malloc(manifest_size);
     if (buffer != nullptr) {
@@ -24,21 +25,27 @@ Manifest::Manifest(oe_uuid_t component_id, const char* manifest, size_t manifest
     }
 }
 
-void Manifest::AddManifest(oe_uuid_t component_id, const char* manifest, size_t manifest_size)
+void Manifest::AddManifest(oe_uuid_t component_id, const char* manifest_content, size_t manifest_content_size)
 {
-    g_Manifest = new Manifest(component_id, manifest, manifest_size);
+    Manifest* manifest = new Manifest(component_id, manifest_content, manifest_content_size);
+    manifest->Next = g_Manifest;
+    g_Manifest = manifest;
+
 }
 
 const char* Manifest::GetManifest(UsefulBufC* component_id, size_t* manifest_size)
 {
-    if (sizeof(g_Manifest->_component_id) != component_id->len) {
-        return nullptr;
+    for (Manifest* manifest = g_Manifest; manifest != nullptr; manifest = manifest->Next) {
+        if (sizeof(g_Manifest->_component_id) != component_id->len) {
+            continue;
+        }
+        if (memcmp(&g_Manifest->_component_id, component_id->ptr, component_id->len) != 0) {
+            continue;
+        }
+        *manifest_size = g_Manifest->_manifest_size;
+        return g_Manifest->_manifest;
     }
-    if (memcmp(&g_Manifest->_component_id, component_id->ptr, component_id->len) != 0) {
-        return nullptr;
-    }
-    *manifest_size = g_Manifest->_manifest_size;
-    return g_Manifest->_manifest;
+    return nullptr;
 }
 
 void ecall_ConfigureManifest(oe_uuid_t component_id, const char* manifest, size_t manifest_size)
--- TeepTamTA/Manifest.h
+++ TeepTamTA/Manifest.h
@@ -2,8 +2,10 @@
 class Manifest
 {
 public:
-    static void AddManifest(oe_uuid_t component_id, const char* manifest, size_t manifest_size);
+    static void AddManifest(oe_uuid_t component_id, const char* manifest_content, size_t manifest_content_size);
     static const char* GetManifest(UsefulBufC* component_id, size_t* manifest_size);
+
+    Manifest* Next;
 
 private:
     Manifest(oe_uuid_t component_id, const char* manifest, size_t manifest_size);
